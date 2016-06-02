ba114457dc2d17ad89cc4188f39d86d3b36bef86
day318 <304469323@qq.com>
2016-06-02 20:20:00 +0800

    add flex

--- gulpfile.js
+++ gulpfile.js
@@ -9,27 +9,27 @@ var sass = require('gulp-sass');
 var sequence = require('gulp-sequence');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
     return gulp.src('dist/')
         .pipe(clean());
 });
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     return gulp.src('./sass/luck.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
         .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
 
     gulp.watch('./sass/**/*', ['sass']);
 
     browserSync.init({
         server: {
             baseDir: "./",
-            index: "doc/coupon.html"
+            index: "test.html"
         }
     });
-    //gulp.watch("dist/**/*").on("change", browserSync.reload);
+    gulp.watch(["dist/**/*", "test.html"]).on("change", browserSync.reload);
 });
