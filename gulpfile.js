a1bd8e977351cd18be4e9fcc59cb3bbe333c5414
day318 <304469323@qq.com>
2016-10-10 15:36:26 +0800

    移除不适合小程序的代码

--- gulpfile.js
+++ gulpfile.js
@@ -4,42 +4,21 @@
 
 var gulp = require("gulp");
 var clean = require("gulp-clean");
-var browserSync = require("browser-sync");
 var sass = require('gulp-sass');
 var sequence = require('gulp-sequence');
 var autoprefixer = require('gulp-autoprefixer');
+var rename = require("gulp-rename");
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
     return gulp.src('dist/')
         .pipe(clean());
 });
 
-gulp.task('luck', function() {
+gulp.task('luck', function () {
     return gulp.src('./sass/luck.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['iOS 7', 'Android >= 4.0','> 1%']
-        }))
-        .pipe(gulp.dest('./dist/'));
-});
-gulp.task('gplqg', function() {
-    return gulp.src('./sass/gplqg.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['iOS 7', 'Android >= 4.0','> 1%']
-        }))
+        .pipe(rename("luck.wxss"))
+        //.pipe(autoprefixer({browsers: ['iOS 7', 'Android >= 4.0', '> 1%']}))
         .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('watch', function() {
-
-    gulp.watch('./sass/**/*', ['sass']);
-
-    browserSync.init({
-        server: {
-            baseDir: "./",
-            index: "doc/luckcss.html"
-        }
-    });
-    gulp.watch(["dist/**/*", "doc/luckcss.html"]).on("change", browserSync.reload);
-});
