54bd2602e9bf03fcf869a8b5830b38ea1a52602b
day318 <304469323@qq.com>
2016-11-03 15:33:01 +0800

    asdf

--- gulpfile.js
+++ gulpfile.js
@@ -6,32 +6,33 @@ var gulp = require("gulp");
 var clean = require("gulp-clean");
 var browserSync = require("browser-sync");
 var sass = require('gulp-sass');
+var less=require('gulp-less');
 var sequence = require('gulp-sequence');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
     return gulp.src('dist/')
         .pipe(clean());
 });
 
-gulp.task('luck', function() {
+gulp.task('luck', function () {
     return gulp.src('./sass/luck.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
-            browsers: ['iOS 7', 'Android >= 4.0','> 1%']
+            browsers: ['iOS 7', 'Android >= 4.0', '> 1%']
         }))
         .pipe(gulp.dest('./dist/'));
 });
-gulp.task('gplqg', function() {
+gulp.task('gplqg', function () {
     return gulp.src('./sass/gplqg.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
-            browsers: ['iOS 7', 'Android >= 4.0','> 1%']
+            browsers: ['iOS 7', 'Android >= 4.0', '> 1%']
         }))
         .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
 
     gulp.watch('./sass/**/*', ['sass']);
 
@@ -42,4 +43,13 @@ gulp.task('watch', function() {
         }
     });
     gulp.watch(["dist/**/*", "doc/luckcss.html"]).on("change", browserSync.reload);
+});
+
+gulp.task('less', function () {
+    return gulp.src('./less/test.less')
+        .pipe(less())
+        .pipe(autoprefixer({
+            browsers: ['iOS 7', 'Android >= 4.0', '> 1%']
+        }))
+        .pipe(gulp.dest('./dist/'));
 });
